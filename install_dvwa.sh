12e96b42c00eb15bc9e7e611bc432fac33135614
Cole <tbaty002@odu.edu>
2023-05-04 15:51:26 -0400

    ported lxc install script

--- install_dvwa.sh
+++ install_dvwa.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
+# automated installation of DVWA
+# 
+# target is a LAMP stack running DVWA
+#
+# script helps automate process
+# must be run as root
 
-# check for required packages
-# clone DVWA from git to /tmp
-# copy to <ServerRoot>/DVWA
-# chown -R www-data:www-data <ServerRoot>/DVWA/*
-# initialize database
+if [[ `whoami` -ne "root" ]]; then
+    echo "must be run as root; quitting"
+    return 1
+fi
+
+WORKING_DIR=/var/www
+DVWA_GH=https://github.com/digininja/DVWA.git
+
+source /root/secrets.sh
+rm -f /root/secrets.sh
+
+#REQUIREMENTS=("apache2" "libapache2-mod-php" "mariadb-server" "mariadb-client" "php7.4" "php7.4-mysqli" "php7.4-gd")
+
+#apk --no-cache --update add ${REQUIREMENTS}
+
+echo "[+] installing required packages"
+apt-get update -y -q && apt-get install -y -q \
+	git \
+	apache2 libapache2-mod-php \
+	mariadb-server mariadb-client \
+	php7.4 php7.4-mysqli php7.4-gd  
+
+echo "[+] setting up DVWA"
+echo "[+] cloning DVWA"
+cd ${WORKING_DIR}
+git clone ${DVWA_GH}
+rm -rf ${WORKING_DIR}/html/*
+mv ${WORKING_DIR}/DVWA/* ${WORKING_DIR}/html/
+chown -R www-data:www-data ${WORKING_DIR}/html/*
+rm -rf ${WORKING_DIR}/DVWA
+
+DVWA_DIR=${WORKING_DIR}/html
+
+cp ${DVWA_DIR}/config/config.inc.php.dist ${DVWA_DIR}/config/config.inc.php
+
+
+echo "[+] setting up database"
+mysql < /root/mysql_commands
+
